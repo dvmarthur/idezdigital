16e5aa99bbc9fea9d2d06959affb2fb286e18d93
Arthur Dias <arthurdvm2@gmail.com>
2023-03-24 10:22:25 -0300

    implementando sistema de cache laravel

--- app/Http/Controllers/MunicipiosController.php
+++ app/Http/Controllers/MunicipiosController.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Services\IbgeService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class MunicipiosController extends Controller
 {
     public function index(IbgeService $ibgeService, $estado)
     {
-        $municipios = $ibgeService->getMunicipios($estado);
-    
-        return response()->json($municipios);
+        $cacheKey = 'municipios-' . $estado;
+
+        $municipiosSimplificados = Cache::remember($cacheKey, $minutes = 60, function () use ($ibgeService, $estado) {
+            $municipios = $ibgeService->getMunicipios($estado);
+            return array_map(function($municipio) {
+                return ['id' => $municipio['id'], 'nome' => $municipio['nome']];
+            }, $municipios);
+        });
+        
+        return response()->json($municipiosSimplificados);
     }
 }
